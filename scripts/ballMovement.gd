670bfb55328a91f27966d4e0df4094c23301657e
Oliver Barclay <oliver.barclay@gmail.com>
2025-02-25 18:57:47 +1300

    Update ballMovement.gd
    Implemented when user inputs vector is greater than pi/2 (90 degrees) the momentum is reset (simulate not being able to turn on a die)

--- scripts/ballMovement.gd
+++ scripts/ballMovement.gd
@@ -9,6 +9,7 @@ var max_velocity: float = 4000 # Max velocity the ball can reach
 var multix: float = 0 # Momentum Multiplier
 # Movement vector
 var velocity_vector: Vector2 = Vector2.ZERO
+var speed = 0
 
 # Previous Input Vector
 var current_vector = Vector2.ZERO
@@ -17,15 +18,18 @@ var current_vector = Vector2.ZERO
 var momentum_accumulator: float = 0.0
 var momentum_increase_rate: float = 50.0 # The rate at which momentum increases
 
+## Builds speed depending on the input vector (direction player wants to move) & momentum rate
+## Resets momentum rate if reset = 1 (e.g. in cases where ball reaches slow sleep or player attempts turn around)
 func buildSpeed(input_vector, reset):
 	if reset == 1:
 		momentum_accumulator = 0.0 # Reset momentum when there's no input
+		speed = BASE_SPEED
 	else:
 		# Increase momentum over time when input is held
 		momentum_accumulator += momentum_increase_rate * get_process_delta_time()
 	
 	# Increase speed based on momentum
-	var speed = BASE_SPEED + momentum_accumulator
+	speed = BASE_SPEED + momentum_accumulator
 	return input_vector * speed
 
 func _physics_process(delta):
@@ -48,7 +52,15 @@ func _physics_process(delta):
 	# If there's input, apply force in that direction
 	if input_vector != Vector2.ZERO:
 		current_vector = input_vector  # Update current_vector with new input
-		velocity_vector = velocity_vector.lerp(buildSpeed(input_vector, 0), 0.1)
+
+		if input_vector.angle_to(velocity_vector) > (PI/2) || input_vector.angle_to(velocity_vector) < -(PI/2):
+			velocity_vector = velocity_vector.lerp(buildSpeed(input_vector, 1), 0.1)
+		else:
+			velocity_vector = velocity_vector.lerp(buildSpeed(input_vector, 0), 0.1)
+	# Checks if moving slower than 50 units in all directions to reset momentum
+	elif (velocity_vector.x < 50 && velocity_vector.x > -50) && (velocity_vector.y < 50 && velocity_vector.y > -50) :
+		buildSpeed(input_vector, 1)
+		velocity_vector *= (1 - friction)
 	else:
 		#velocity_vector = velocity_vector.lerp(Vector2.ZERO, 0.1)
 		# Apply friction to slow down the ball naturally when there's no input
